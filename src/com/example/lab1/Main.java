2fb41b3f23d3f97b8265a6cfcc1c529faf0428cb
hong <21700794@handong.edu>
2021-09-04 14:45:20 +0900

    Second Commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -10,7 +10,8 @@ public class Main {
 		System.out.println("Hello World!!!\n");
 		System.out.print("이름 입력 : ");
 		String name = s.next();
-		System.out.println("[" + name + "]님 환영합니다~");
+		System.out.println("[" + name + "]님");
+		System.out.println("환영합니다!!");
 	}
 
 }
